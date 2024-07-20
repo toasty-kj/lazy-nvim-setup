c8c7ef63cbb8e93b0ec173834492d2d92d703b73
toasty-kj <pr5r1uqr@s.okayama-u.ac.jp>
2024-07-21 03:22:31 +0900

    surrounding と telescopeのショートカットを変更

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,6 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
       keys = {
-	       { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+	       { "<leader>p", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       },
     }
